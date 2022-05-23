45efbb4925d2fb6d06e4d7ed278f5a88481e5d48
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 09:50:41 +0700

    Create a string from an array

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// 'fruits' array created using array literal notation
 const fruits = ['Apple', 'Banana'];
-console.log(fruits.length);
-// 2
-
-// 'fruits2' array created using the Array() constructor.
-const fruits2 = new Array('Apple', 'Banana');
-console.log(fruits2.length);
-// 2
-
-// 'fruits3' array created using the String.prototype.split().
-const fruits3 = 'Apple, Banana'.split(', ');
-console.log(fruits3.length);
-// 2
+const fruitsString = fruits.join(', ');
+console.log(fruitsString);
+// "Apple, Banana"

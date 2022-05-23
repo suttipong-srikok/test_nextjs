5836117ac58b7842251e98e81801ad4c9a35e7b5
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 11:01:02 +0700

    Replace multiple items in an array

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fruits = ['Apple', 'Banana', 'Strawberry', 'Mango'];
-const start = 1;
+const fruits = ['Apple', 'Banana', 'Strawberry'];
+const start = -2;
 const deleteCount = 2;
-const removedItems = fruits.splice(start, deleteCount);
-console.log(fruits); // ['Apple', 'Mango']
-console.log(removedItems); // ['Banana', 'Strawberry']
+const removedItems = fruits.splice(start, deleteCount, 'Mango', 'Cherry');
+console.log(fruits); // ['Apple', 'Mango', 'Cherry']
+console.log(removedItems); // ['Banana', 'Strawberry]

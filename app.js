618e6de726886067c82ded49d48ab0fce7c25c57
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 11:03:55 +0700

    Iterate over an array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fruits = ['Apple', 'Banana', 'Strawberry'];
-const start = -2;
-const deleteCount = 2;
-const removedItems = fruits.splice(start, deleteCount, 'Mango', 'Cherry');
-console.log(fruits); // ['Apple', 'Mango', 'Cherry']
-console.log(removedItems); // ['Banana', 'Strawberry]
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+// Apple
+// Banana
+// Strawberry

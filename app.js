331444550520b83bd92191b16ef7f74396756cd3
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 10:11:53 +0700

    Append an item to an array

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 const fruits = ['Apple', 'Banana'];
-
-var b = fruits.includes('Banana'); 
-console.log(b); // true
-
-b = fruits.includes('Cherry');
-console.log(b); // false
-
-// If indexOf() doesn't return -1, the array contains the given item.
-b = fruits.indexOf('Banana') !== -1;
-console.log(b); // true;
-
-b = fruits.indexOf('Cherry') !== -1;
-console.log(b); // false
+const newLength = fruits.push('Orange');
+console.log(fruits); // ["Apple", "Banana", "Orange"]
+console.log(newLength); // 3

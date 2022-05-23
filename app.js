c2e4487e07bc0f25502e751ebbab907c475401b7
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 10:26:26 +0700

    Remove the first item from an array

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const fruits = ['Apple', 'Banana', 'Strawberry', 'Mango', 'Cherry'];
-const start = 2;
-const removedItems = fruits.splice(start);
-console.log(fruits); // ['Apple', 'Banana']
-console.log(removedItems); // ['Strawberry', 'Mango', 'Cherry']
+const fruits = ['Apple', 'Banana', 'Strawberry'];
+const removedItem = fruits.shift();
+console.log(fruits); // ['Banana', 'Strawberry']
+console.log(removedItem); // Apple

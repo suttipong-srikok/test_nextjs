0e5dccc69ba0dec62eda0250b3b31c60d42d11c2
Suttipong Srikok <suttipongs@toptrader.co.th>
2022-05-23 09:59:40 +0700

    Access an array item by its index

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const fruits = ['Apple', 'Banana'];
-const fruitsString = fruits.join(', ');
-console.log(fruitsString);
-// "Apple, Banana"
+
+// The index of an array's first element is always 0.
+let f1 = fruits[0]; // Apple
+console.log(f1);
+
+// The index of an array's second element is always 1.
+f1 = fruits[1]; //Banana
+console.log(f1);
+
+// The index of an array's last element is always 
+// one less than the length of the array
+f1 = fruits[fruits.length - 1]; // Banana
+console.log(f1);
+
+// Using a index number larger than the array's length return 'undefined'.
+f1 = fruits[99]; // undefined
+console.log(f1);
